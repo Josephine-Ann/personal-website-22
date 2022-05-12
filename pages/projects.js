b332af64c159c146d7e57b3268767408c522c4b9
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-12 12:35:51 +0200

    alt and errors around unescaped entities

--- pages/projects.js
+++ pages/projects.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHandPeace, faArrowAltCircleDown, faTimeline, faPalette, faPuzzlePiece, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { useThemes } from '../context/themeContext';
 import useScrollPosition from '@react-hook/window-scroll'
-import { useMouseWheel } from 'react-use';
+import { useMouseWheel, useWindowSize } from 'react-use';
 
 export default function Projects() {
     const {
@@ -18,6 +18,7 @@ export default function Projects() {
         imagesProjectsPageAlt,
         carouselClasses
     } = useThemes()
+    const { width, height } = useWindowSize();
     const scrollY = useScrollPosition(60 /*frames per second*/);
 
     const PhotoWrapperProjectIcons = ({ children }) => {
@@ -26,11 +27,11 @@ export default function Projects() {
 
     return (
         <Navbar>
-            <div className={`${scrollY >= window.innerHeight ? scrollClasses[1] : scrollClasses[2]}`}>
+            <div className={`${scrollY >= height ? scrollClasses[1] : scrollClasses[2]}`}>
                 <div className='text-center w-full h-[100vh] flex flex-col justify-center items-center lato stan_mob:text-2xl md:text-[46px] tab:text-4xl text-lg'>
                     <h1 className='tab:px-8 px-4'>Would you like to take a look at other things I&apos;ve done?</h1>
                     <button onClick={() => {
-                        window.scrollTo(0, window.innerHeight)
+                        window.scrollTo(0, height)
                     }} className='flex justify-center items-center w-[55vw] tab:w-[35vw] lan:w-[25vw] my-5 tab:text-4xl md:text-[46px] px-3 py-4 bg-[#27292C] text-white'>Let&apos;s go!<FontAwesomeIcon className="ml-2" icon={faArrowAltCircleDown} /></button>
                 </div>
                 <div className='items-center w-full flex flex-col big_mob:flex-row h-[100vh] min-h-fit'>
