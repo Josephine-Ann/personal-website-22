2c9526a439f1406561578a1611e359a6ffbf2a78
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-12 17:43:43 +0200

    change location tailwind package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,29 @@
+
+
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
 }
 
+// module.exports = {
+//   plugins: {
+//     'postcss-import': {},
+//     'tailwindcss/nesting': {},
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
