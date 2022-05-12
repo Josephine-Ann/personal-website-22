12714ddecd8e385246ffa9e8be1437aab3b50c2e
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-12 15:27:16 +0200

    postcssconfig

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
