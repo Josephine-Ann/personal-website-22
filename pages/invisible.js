f7a934c1bc615a374966705b3675655fa6052ee4
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-13 10:25:32 +0200

    invisible

---
+++ pages/invisible.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from "../Components/Navbar";
+
+
+export default function Invisible() {
+    return (
+        <Navbar>
+            <div className="h-[420px] w-[460px] mx-3">
+                <div className="h-[10px] tab:justify-end tab:bg-transparent tab:flex-row">
+
+                </div>
+                <div className="h-[8px] w-[400px]">
+
+                </div>
+            </div>
+        </Navbar>
+    )
+}
+
+
