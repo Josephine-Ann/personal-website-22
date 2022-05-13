dc729f77c9805a6c5fed1590a7fc8a0934433b86
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-13 10:38:48 +0200

    invisible

--- pages/invisible.js
+++ pages/invisible.js
@@ -5,11 +5,11 @@ import Navbar from "../Components/Navbar";
 export default function Invisible() {
     return (
         <Navbar>
-            <div className="h-[420px] w-[460px] mx-3">
-                <div className="h-[10px] tab:justify-end tab:bg-transparent tab:flex-row">
+            <div className="tab:h-[420px] tab:w-[460px] tab:mx-3">
+                <div className="tab:h-[10px] tab:justify-end tab:bg-transparent tab:flex-row">
 
                 </div>
-                <div className="h-[8px] w-[400px]">
+                <div className="tab:h-[8px] tab:w-[400px]">
 
                 </div>
             </div>
