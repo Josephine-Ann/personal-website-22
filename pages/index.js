bcf9411fbcef8999f256f891d98bab186bf181b1
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-14 15:53:49 +0200

    favicon

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <Navbar>
       <Head>
+        <title>Josephine Geoghegan</title>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <PhotoWrapper>
