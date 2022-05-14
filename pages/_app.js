dc8e053cf044ee53ec8aff40b78abe2e08afc8f6
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-14 16:14:04 +0200

    scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { ThemeProvider } from '../context/themeContext'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <ThemeProvider>
       <Component {...pageProps} />
